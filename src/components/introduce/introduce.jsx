5ffde3bf798f31bb40100db1403c26e510bbb535
Luis Tovar <ltovaralc@gmail.com>
2024-06-18 20:48:37 -0600

    animation de entrada agregada

--- src/components/introduce/introduce.jsx
+++ src/components/introduce/introduce.jsx
@@ -1,17 +1,26 @@
 /* Introduccion de mi como desarrollador */
 import "./introduce.css";
 import ProfilePic from "../../assets/profilePic.png";
+import "animate.css";
 
 const Introduce = () => {
   return (
     <section className="introduce">
-      <img
-        src={ProfilePic}
-        alt="LtovarDEV profilePicDeveloper DeveloperAnime"
-        className="profilePic"
-      />
+      <div className="introduce-gretting">
+        <img
+          src={ProfilePic}
+          alt="LtovarDEV profilePicDeveloper DeveloperAnime"
+          className="profilePic"
+        />
+        <div className="gretting-text animate__animated animate__fadeInRight animate__delay-1s">
+          <span className="Hey">Hey</span>
+          <p className="name">, I'm Luis Tovar</p>
+        </div>
+      </div>
       <h1 className="introduce-title">Front-End Developer</h1>
-      <a href="#aboutMe"><button className="button">About me</button></a>
+      <a href="#aboutMe">
+        <button className="button">About me</button>
+      </a>
     </section>
   );
 };
