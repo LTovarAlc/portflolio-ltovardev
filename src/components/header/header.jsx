0cb87646c9bdd43a931d381a5beb6a8b1859afa9
Luis Tovar <ltovaralc@gmail.com>
2024-06-20 14:38:07 -0600

    darkmode en header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,22 +1,43 @@
 /* Header del portfolio */
 
-import './header.css'
+import "./header.css";
+import React, { useState } from "react";
 
 const Header = () => {
-    return (
-        <header className='header'>
-            <ul className='header-list'>
-            <a href="#aboutMe"><li className="menu-list__item-link">About Me</li></a>
-            <a href="#projects"><li className="menu-list__item-link">Projects</li></a>
-            <a href="#skills"><li className="menu-list__item-link">Skills</li></a>
-            <a href=""><li className="menu-list__item-link">Contact</li></a>
-            <label className="switch">
-                <input type="checkbox"/>
-                <span className="slider"></span>
-            </label>
-            </ul>
-        </header>
-    )
-}
+  const [darkMode, setDarkMode] = useState(false);
 
-export default Header
+  const toggleDarkMode = () => {
+    if (darkMode === false) {
+      setDarkMode(true);
+      console.log("modo oscuro");
+    } else {
+      setDarkMode(false);
+      console.log("modo claro");
+    }
+  };
+  
+  return (
+    <header  className={`header ${darkMode ? 'header-dark' : ''}`}>
+      <ul className={`header-list ${darkMode ? 'header-list__dark' : ''}`}>
+        <a href="#aboutMe">
+          <li className="menu-list__item-link">About Me</li>
+        </a>
+        <a href="#projects">
+          <li className="menu-list__item-link">Projects</li>
+        </a>
+        <a href="#skills">
+          <li className="menu-list__item-link">Skills</li>
+        </a>
+        <a href="">
+          <li className="menu-list__item-link">Contact</li>
+        </a>
+        <label className="switch">
+          <input type="checkbox" onClick={toggleDarkMode} />
+          <span className="slider"></span>
+        </label>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
